afa0b603c06e99bc452edfdf23a770429b8bde5f
6ocra3 <6o3kmm@mail.ru>
2024-12-19 16:34:50 +0300

    init

---
+++ src/test/java/org/makar/logstarter/LogStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.makar.logstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
